56922b46c68c99b74b3ee88e31d1645d2cc594a8
Mark Norgren <mark@marknorgren.com>
2023-02-02 12:16:30 -0600

    Initial Commit

---
+++ SwiftUIDemos/SwiftUIDemosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemosApp.swift
+//  SwiftUIDemos
+//
+//  Created by mark on 2/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
